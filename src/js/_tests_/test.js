91fab96f7bb9ee3b85feed32f3127b44374f0858
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-01-30 23:52:33 +0600

    первый вариант теста

--- src/js/_tests_/test.js
+++ src/js/_tests_/test.js
@@ -1,6 +1,6 @@
 import { lifePlayer } from "../app.js";
 
-/* test("lifePlayer", () => {
+test("lifePlayer", () => {
   const player = { name: "Маг", health: 90 };
   const result = lifePlayer(player);
   expect(result).toBe("healthy");
@@ -14,13 +14,13 @@ test("lifePlayer", () => {
   const player = { name: "Маг", health: 10 };
   const result = lifePlayer(player);
   expect(result).toBe("critical");
-}); */
+});
 
-test.each([
+/* test.each([
   [{ name: "Маг", health: 90 }, "healthy"],
   [{ name: "Маг", health: 40 }, "wounded"],
   [{ name: "Маг", health: 10 }, "critical"],
 ])("testing life player %s status and %i amount", (amount, exp) => {
   const result = lifePlayer(amount);
   expect(result).toBe(exp);
-});
+}); */
